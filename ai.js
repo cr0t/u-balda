4eb15d4917540b225e7c0e54ddff2330f098030c
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2019-02-22 13:31:07 +0400

    Add Benchmark to ai runner script

--- ai.js
+++ ai.js
@@ -1,3 +1,5 @@
+import Benchmark from 'benchmark';
+
 const Vocabulary = require('./lib/Vocabulary').default;
 const ArtificialIntelligence = require('./lib/ArtificialIntelligence').default;
 
@@ -7,12 +9,55 @@ const vocabulary = new Vocabulary();
 vocabulary.loadWordsData(DATA);
 const ai = new ArtificialIntelligence(vocabulary);
 
-const cells = [
-  '',  'о', 'к', '',  '',
+const cells3 = [
+  '',  '',  '',
+  'р', 'а', 'б',
+  '',  '',  '',
+];
+
+const cells4 = [
+  '',  '',  '',  '',
+  'п', 'л', 'о', 'в',
+  '',  '',  '',  '',
+  '',  '',  '',  '',
+];
+
+const cells5 = [
+  '',  '',  '',  '',  '',
+  '',  '',  '',  '',  '',
+  'б', 'а', 'л', 'д', 'а',
+  '',  '',  '',  '',  '',
+  '',  '',  '',  '',  '',
+];
+
+const cellsComplex = [
+  '',  '',  'к', '',  '',
   '',  'к', 'о', 'о', '',
   'б', 'а', 'ч', 'к', 'и',
   '',  'к', '',  'а', 'м',
   '',  'т', '',  '',  '',
 ];
 
-console.log(ai.findWord(cells));
+// Without any optimizations:
+// findWord#3x3 x 66.20 ops / sec ±2.33 % (64 runs sampled)
+// findWord#4x4 x 21.52 ops / sec ±2.56 % (38 runs sampled)
+// findWord#5x5 x 10.59 ops / sec ±2.29 % (30 runs sampled)
+// findWord#5x5 - complex x 0.13 ops / sec ±1.76 % (5 runs sampled)
+// Fastest is findWord#3x3
+// ✨  Done in 95.30s.
+
+const suite = new Benchmark.Suite;
+
+suite.add('findWord#3x3', function () {
+  ai.findWord(cells3);
+}).add('findWord#4x4', function () {
+  ai.findWord(cells4);
+}).add('findWord#5x5', function () {
+  ai.findWord(cells5);
+}).add('findWord#5x5-complex', function () {
+  ai.findWord(cellsComplex);
+}).on('cycle', function (event) {
+  console.log(String(event.target));
+}).on('complete', function () {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+}).run({ 'async': true });

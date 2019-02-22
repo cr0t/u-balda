8870f80357844e2d2528798b2fe3ace7d640734d
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2019-02-22 15:00:54 +0400

    Optimize Vocabulary with Set for words storage

--- ai.js
+++ ai.js
@@ -42,9 +42,17 @@ const cellsComplex = [
 // findWord#3x3 x 66.20 ops / sec ±2.33 % (64 runs sampled)
 // findWord#4x4 x 21.52 ops / sec ±2.56 % (38 runs sampled)
 // findWord#5x5 x 10.59 ops / sec ±2.29 % (30 runs sampled)
-// findWord#5x5 - complex x 0.13 ops / sec ±1.76 % (5 runs sampled)
+// findWord#complex x 0.13 ops / sec ±1.76 % (5 runs sampled)
 // Fastest is findWord#3x3
 // ✨  Done in 95.30s.
+
+// After changing way of storing words in Vocabulary to Set()
+// findWord#3x3 x 2, 848 ops / sec ±0.62 % (86 runs sampled)
+// findWord#4x4 x 1, 466 ops / sec ±0.46 % (91 runs sampled)
+// findWord#5x5 x 813 ops / sec ±2.75 % (83 runs sampled)
+// findWord#complex x 22.77 ops / sec ±2.38 % (40 runs sampled)
+// Fastest is findWord#3x3
+// ✨  Done in 24.91s.
 
 const suite = new Benchmark.Suite;
 
@@ -54,7 +62,7 @@ suite.add('findWord#3x3', function () {
   ai.findWord(cells4);
 }).add('findWord#5x5', function () {
   ai.findWord(cells5);
-}).add('findWord#5x5-complex', function () {
+}).add('findWord#complex', function () {
   ai.findWord(cellsComplex);
 }).on('cycle', function (event) {
   console.log(String(event.target));
